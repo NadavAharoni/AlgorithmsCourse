fc26efc362b2525109a1be2277c006ecfe0b6120
Nadav Aharoni <nadav.a@fluenttech.net>
2023-12-01 00:32:04 +0200

    completed and tested mergesort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,26 @@
 def merge_sort(array):
     if len(array) <= 1:
         return
-    mid = int(len(array/1))
-    # sort each half, then merge ..
+    mid = int(len(array)/2)
+    # sort each half, then merge
+    array_a = array[:mid]
+    merge_sort(array_a)
+    array_b = array[mid:]
+    merge_sort(array_b)
+    print(F"array_a={array_a}")
+    print(F"array_b={array_b}")
+    # merge
+    for i in range(0,len(array)):
+        if len(array_b)==0:
+            array[i] = array_a.pop(0)
+        elif len(array_a)==0:
+            array[i] = array_b.pop(0)
+        else: # both arrays must have a non-zero length
+            if array_a[0] <= array_b[0]:
+                array[i] = array_a.pop(0)
+            else:
+                array[i] = array_b.pop(0)
+
 
 def insertion_sort(array):
     # array[0] is already "sorted" so we start with array[1]
@@ -27,8 +45,9 @@ def read_from_file(array):
         num_str = line.strip()
         array.append(int(num_str))
 
-numbers = [300,200,500,100,400]
+numbers = [999,300,200,500,800,400,900,100,700]
 # numbers = []
 # read_from_file(numbers)
-insertion_sort(numbers)
-print(numbers)
+# insertion_sort(numbers)
+merge_sort(numbers)
+print(F"sorted:{numbers}")
