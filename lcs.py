2f93f6f3d99cb38eaf1e124aa8a58697e3134a53
Nadav Aharoni <nadav.a@fluenttech.net>
2024-01-28 19:26:35 +0200

    Moved Matrix2D into a separate file

--- lcs.py
+++ lcs.py
@@ -1,3 +1,5 @@
+from matrix2d import Matrix2D
+
 recursive_lcs_counter=0
 recursive_lcs_memo_counter = 0
 
@@ -20,47 +22,6 @@ def recursive_lcs(x : list, y: list):
         else:
             lcs = lcs2
     return lcs
-
-class Matrix2D:
-    def __init__(self, rows: int = 0, columns : int = 0, value = None):
-        self.m = []
-        self.m.extend([[] for _ in range(rows)])
-        for row in self.m:
-            row.extend([value for _ in range(columns)])
-
-    def check_extend(self, i, j):
-        if len(self.m) <= i:
-            num_extra_rows = i + 1 - len(self.m)
-            self.m.extend([[] for _ in range(num_extra_rows)])
-        row = self.m[i]
-        if len(row) <= j:
-            num_extra_columns = j + 1 - len(row)
-            extra = [None for _ in range(num_extra_columns)]
-            row.extend(extra)
-
-    def __getitem__(self, ij : tuple):
-        i = ij[0]
-        j = ij[1]
-        self.check_extend(i,j)
-        return self.m[i][j]
-
-    def __setitem__(self, ij : tuple, value):
-        i = ij[0]
-        j = ij[1]
-        self.check_extend(i,j)
-        self.m[i][j] = value
-
-    def __str__(self) -> str:
-        ret = ""
-        line_num = 0
-        for i in self.m:
-            ret += F"{line_num}:"
-            for j in i:
-                ret += F"{j}, "
-            ret += "\n"
-            line_num += 1
-        return ret
-
 
 def recursive_lcs_memo(x : list, y: list, m : Matrix2D):
     global recursive_lcs_memo_counter
