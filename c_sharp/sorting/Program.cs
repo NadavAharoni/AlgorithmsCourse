49ce505fa05197bf8aca5ab008eb47ff61bef9a6
NadavAharoni <65665296+NadavAharoni@users.noreply.github.com>
2024-03-02 23:41:39 +0200

    Update Program.cs
    Initialize and print a list of integers

--- c_sharp/sorting/Program.cs
+++ c_sharp/sorting/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> list = new List<int>() { 23, 26, 4, -5, 12, 35, 122 };
+            list.Add(17);
+            list.Add(26);
+            list.Add(39);
+
+            for (int i = 0; i < list.Count-1; i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+            Console.WriteLine($"{list[list.Count - 1]}");
+            Console.WriteLine($"{list.Count}");
         }
     }
 }
