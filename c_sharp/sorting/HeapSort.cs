486e5d632114b8f87b3da6bd75e351ff7b4e4792
NadavAharoni <65665296+NadavAharoni@users.noreply.github.com>
2024-03-03 11:51:21 +0200

    Created HeapSort.cs

---
+++ c_sharp/sorting/HeapSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sorting
+{
+    public class HeapSort
+    {
+        public static void MaxHeapify(List<int> list, int root)
+        {
+            int left = Left(root);
+            int right = Right(root);
+
+            Console.WriteLine($"root={root}, left={left}, right={right}, list length={list.Count}");
+
+        }
+ 
+        public static int Left(int i)
+        {
+            int left = 2 * i;
+            return left; 
+        }
+
+        public static int Right(int i)
+        {
+            int right = 2 * i + 1;
+            return right;
+        }
+
+    }
+}
