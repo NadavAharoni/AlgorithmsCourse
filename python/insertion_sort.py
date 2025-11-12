71e44f0bd37a3cdc3c36b8b22da12223f4b968c9
Nadav Aharoni <65665296+NadavAharoni@users.noreply.github.com>
2025-11-13 00:43:16 +0200

    Moved mergesort into its own file

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,27 +1,3 @@
-def merge_sort(array):
-    if len(array) <= 1:
-        return
-    mid = int(len(array)/2)
-    # sort each half, then merge
-    array_a = array[:mid]
-    merge_sort(array_a)
-    array_b = array[mid:]
-    merge_sort(array_b)
-    print(F"array_a={array_a}")
-    print(F"array_b={array_b}")
-    # merge
-    for i in range(0,len(array)):
-        if len(array_b)==0:
-            array[i] = array_a.pop(0)
-        elif len(array_a)==0:
-            array[i] = array_b.pop(0)
-        else: # both arrays must have a non-zero length
-            if array_a[0] <= array_b[0]:
-                array[i] = array_a.pop(0)
-            else:
-                array[i] = array_b.pop(0)
-
-
 def insertion_sort(array):
     # array[0] is already "sorted" so we start with array[1]
     next_item_index=1
@@ -48,7 +24,6 @@ numbers = [999,300,200,500,800,400,900,100,700]
 # numbers = []
 # read_from_file(numbers)
 # insertion_sort(numbers)
-merge_sort(numbers)
-
+insertion_sort(numbers)
 print(F"sorted:{numbers}")
 
---
+++ python/merge_sort.py
@@ -0,0 +1,35 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return
+    mid = int(len(array)/2)
+    # sort each half, then merge
+    array_a = array[:mid]
+    array_b = array[mid:]
+
+    # print the sub arrays for learning purposes
+    print(F"array_a={array_a}")
+    print(F"array_b={array_b}")
+    print()
+
+    merge_sort(array_a)
+    merge_sort(array_b)
+    # merge
+    for i in range(0,len(array)):
+        if len(array_b)==0:
+            array[i] = array_a.pop(0)
+        elif len(array_a)==0:
+            array[i] = array_b.pop(0)
+        else: # both arrays must have a non-zero length
+            if array_a[0] <= array_b[0]:
+                array[i] = array_a.pop(0)
+            else:
+                array[i] = array_b.pop(0)
+
+
+# main
+numbers = [999,300,200,500,800,400,900,100,700]
+# numbers = []
+# read_from_file(numbers)
+# insertion_sort(numbers)
+merge_sort(numbers)
+print(F"sorted:{numbers}")
