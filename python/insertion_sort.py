8bc79f7feef688b60b47f4bfc8eec98f6e34cc20
NadavAharoni <nadav.aharoni@gmail.com>
2025-11-19 20:12:11 +0200

    Added a key function as argument
    Also:
    Use the function to sort a list of random tuples.
    Added a tuple with an identical x[0] to demonstrate
    tuples sort order

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,10 +1,12 @@
-def insertion_sort(array):
+import random_tuples
+
+def insertion_sort(array, key = lambda x: x):
     # array[0] is already "sorted" so we start with array[1]
     next_item_index=1
     while next_item_index < len(array):
         item_to_insert = array[next_item_index]
         i = next_item_index-1
-        while i >= 0 and array[i] > item_to_insert:
+        while i >= 0 and key(array[i]) > key(item_to_insert):
             array[i+1] = array[i]
             i -= 1
         # assign to array[i+1] because we decrement i
@@ -15,15 +17,22 @@ def insertion_sort(array):
 
 def read_from_file(array):
     f = open("data.txt","r")
-
     for line in f:
         num_str = line.strip()
         array.append(int(num_str))
 
-numbers = [999,300,200,500,800,400,900,100,700]
-# numbers = []
-# read_from_file(numbers)
-# insertion_sort(numbers)
-insertion_sort(numbers)
-print(F"sorted:{numbers}")
+
+a = random_tuples.create_random_tuples(6, 3, [str, str, int])
+t0 = a[0]
+t1 = (t0[0],"wwwww", 134)
+a.append(t1)
+for t in a:
+    print(t)
+
+key=lambda x: x[2] % 100
+
+insertion_sort(a)
+print(F"sorted:")
+for t in a:
+    print(t)
 
