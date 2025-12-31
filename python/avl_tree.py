932cb10535fa0915e5b3b0fe2684241d0db16b19
NadavAharoni <nadav.aharoni@gmail.com>
2025-12-31 10:55:03 +0200

    Corrected type for type checking

--- python/avl_tree.py
+++ python/avl_tree.py
@@ -3,9 +3,9 @@ from typing import Optional
 class AVLNode:
     def __init__(self, value):
         self.value = value
-        self.left : Optional[Node] = None
-        self.right : Optional[Node] = None
-        self.parent : Optional[Node] = None
+        self.left : Optional[AVLNode] = None
+        self.right : Optional[AVLNode] = None
+        self.parent : Optional[AVLNode] = None
         self.balance_factor = 0
         
 
